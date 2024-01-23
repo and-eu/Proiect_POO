d53e41c2e8a9dd68be1c5db843843a9c25715d6d
Florin Angeru <florinangeru@gmail.com>
2024-01-23 20:02:57 +0200

    corecturi si mici modificari

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "Librarii/Produs.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream f("../produs.in");
+    Produs prod;
+    string n;
+    f >> n;
+    cout << n << endl;
+    prod.afisare();
+
+    ofstream g("../produs.out");
     return 0;
 }
