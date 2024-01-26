169d049ea4e5998d6195dd730147bab7940c0b03
Florin Angeru <florinangeru@gmail.com>
2024-01-26 16:09:12 +0200

    implementare clasa Procesor completa

--- Librarii/Procesor.cpp
+++ Librarii/Procesor.cpp
@@ -1,4 +1,37 @@
 #include "Procesor.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int Procesor::index_cod = 1;
+int Procesor::nrProduseDistincte = 0;
+
+
+//constructor cu toate argumentele
+Procesor::Procesor(float pret, const string &model, int an_fabricatie, int garantie, const string &producator,
+                   const string &taraProvenienta, int stoc, string &soclu, int frecventa, int numar_nuclee,
+                   int memorie_cache) : Produs(pret, model, an_fabricatie, garantie, producator, taraProvenienta,
+                                                stoc) {
+    string numar= to_string(index_cod);
+    string zero(4-numar.length(),'0');
+    this->cod_produs = "PS" + zero + numar;
+    this->soclu = soclu;
+    this->frecventa = frecventa;
+    this->numar_nuclee = numar_nuclee;
+    this->memorie_cache = memorie_cache;
+    nrProduseDistincte++;
+    index_cod++;
+}
+
+//destructor
+Procesor::~Procesor() {
+    nrProduseDistincte--;
+}
+
+const string &Procesor::getCodProdus() const {
+    return cod_produs;
+}
 
 const string &Procesor::getSoclu() const {
     return soclu;
@@ -30,4 +63,38 @@ int Procesor::getMemorieCache() const {
 
 void Procesor::setMemorieCache(int memorieCache) {
     memorie_cache = memorieCache;
+}
+
+//afisare
+void Procesor::afisare() const {
+    cout << "Cod produs: " << cod_produs << endl;
+    Produs::afisare();
+    cout << "Soclu: " << soclu << endl;
+    cout << "Frecventa: " << frecventa << endl;
+    cout << "Numar nuclee: " << numar_nuclee << endl;
+    cout << "Memorie cache: " << memorie_cache << endl;
+}
+
+void Procesor::outputFisier(ofstream &stream, char sep) const {
+    stream<< cod_produs << sep;
+    Produs::outputFisier(stream, sep, false);
+    stream << soclu << sep << frecventa << sep << numar_nuclee << sep << memorie_cache << endl;
+}
+
+void Procesor::afisarePretDupaProducator(string &producatorCautat) {
+    if (producatorCautat == this->getProducator()) {
+        cout << "Cod produs: " << this->cod_produs << endl;
+        cout << "Model: " << this->getModel() << endl;
+        cout << "Pret: " << this->getPret() << "lei" << endl;
+        cout << "Stoc: " << this->getStoc() << endl;
+    }
+}
+
+float Procesor::calculPretRedus() {
+    if ((this->getStoc()>20) && (this->getPret()>4000))
+        return this->getPret() - this->getPret() * 0.1;
+    else if ((this->getStoc()>0) && (this->getPret()>4000))
+        return this->getPret() - this->getPret() * 0.05;
+    else
+        return this->getPret();
 }
--- Librarii/Procesor.h
+++ Librarii/Procesor.h
@@ -1,35 +1,44 @@
 #include <string>
 #include "Produs.h"
 
+using namespace std;
+
 class Procesor : public Produs {
 private:
+    string cod_produs; //codul produsului
     string soclu; //socket-ul procesorului
     int frecventa; //frecventa procesorului
     int numar_nuclee; //numarul de nuclee ale procesorului
     int memorie_cache; //memoria cache a procesorului
+    static int nrProduseDistincte; //numarul de produse distincte de tip procesor
+    static int index_cod; //indexul codului produsului
 
 public:
-    Procesor(int codProdus, float pret, const string &marca, int an_fabricatie, int garantie, const string &producator,
+    Procesor(float pret, const string &model, int an_fabricatie, int garantie, const string &producator,
              const string &taraProvenienta, int stoc, string &soclu, int frecventa, int numar_nuclee,
-             int memorie_cache) {} //constructor cu toate argumentele
+             int memorie_cache); //constructor cu toate argumentele
 
-    ~Procesor() {} //destructor
+    ~Procesor(); //destructor
 
-    void afisare() const {}
+    void afisare() const;
+
+    const string &getCodProdus() const;
 
     const string &getSoclu() const;
-
     void setSoclu(const string &soclu);
 
     int getFrecventa() const;
-
     void setFrecventa(int frecventa);
 
     int getNumarNuclee() const;
-
     void setNumarNuclee(int numarNuclee);
 
     int getMemorieCache() const;
-
     void setMemorieCache(int memorieCache);
+
+    void outputFisier(ofstream &stream, char sep = ",") const;
+
+    void afisarePretDupaProducator(string& producatorCautat);
+
+    float calculPretRedus();
 };
