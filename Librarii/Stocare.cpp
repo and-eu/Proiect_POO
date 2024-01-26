983c51732024555e45d8ffcc34492c2b6291d3b5
Florin Angeru <florinangeru@gmail.com>
2024-01-26 16:04:37 +0200

    implementare clasa completa

--- Librarii/Stocare.cpp
+++ Librarii/Stocare.cpp
@@ -1,7 +1,36 @@
 #include "Stocare.h"
+#include <iostream>
+#include <fstream>
 
-namespace std {
-} // std
+using namespace std;
+
+int Stocare::index_cod = 1;
+int Stocare::nrProduseDistincte = 0;
+
+//constructor cu toate argumentele
+Stocare::Stocare(float pret, const string &model, int an_fabricatie, int garantie, const string &producator,
+                 const string &taraProvenienta, int stoc, string &tip, string &format, int capacitate,
+                 int viteza) : Produs(pret, model, an_fabricatie, garantie, producator, taraProvenienta, stoc) {
+    string numar= to_string(index_cod);
+    string zero(4-numar.length(),'0');
+    this->cod_produs = "ST" + zero + numar;
+    this->tip = tip;
+    this->format = format;
+    this->capacitate = capacitate;
+    this->viteza = viteza;
+    nrProduseDistincte++;
+    index_cod++;
+}
+
+//destructor
+Stocare::~Stocare() {
+    nrProduseDistincte--;
+}
+
+const string &Stocare::getCodProdus() const {
+    return cod_produs;
+}
+
 const string &Stocare::getTip() const {
     return tip;
 }
@@ -32,4 +61,41 @@ int Stocare::getViteza() const {
 
 void Stocare::setViteza(int viteza) {
     Stocare::viteza = viteza;
+}
+
+//afisarea stocarii
+void Stocare::afisare() const {
+    cout << "Cod produs: " << cod_produs << endl;
+    Produs::afisare();
+    cout << "Tip: " << tip << endl;
+    cout << "Format: " << format << endl;
+    cout << "Capacitate: " << capacitate << endl;
+    cout << "Viteza: " << viteza << endl;
+}
+
+//afisarea stocarii in fisier
+void Stocare::outputFisier(ofstream &stream, char sep) const {
+    stream << cod_produs << sep;
+    Produs::outputFisier(stream, sep, false);
+    stream << tip << sep << format << sep << capacitate << sep << viteza << endl;
+}
+
+//afisarea pretului dupa producator
+void Stocare::afisarePretDupaProducator(string &producatorCautat) {
+    if (producatorCautat == getProducator()) {
+        cout << "Cod produs: " << this->cod_produs << endl;
+        cout << "Model: " << this->getModel() << endl;
+        cout << "Pret: " << this->getPret() << "lei" << endl;
+        cout << "Stoc: " << this->getStoc() << endl;
+    }
+}
+
+//calcularea pretului redus
+double Stocare::calculPretRedus() {
+    if ((this->getStoc()>20) && (this->getPret()>5000))
+        return this->getPret() - this->getPret() * 0.1;
+    else if ((this->getStoc()>0) && (this->getPret()>5000))
+        return this->getPret() - this->getPret() * 0.05;
+            else
+        return this->getPret();
 }
